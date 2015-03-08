f8e147da3bade53df6810c5d6f2177561e11bd2f
Shaun Hill <shaun.hill@gmail.com>
2015-03-08 16:53:29 -0400

    removed /android/support/v4/ (should be an external dependency), and R.java and Manifest.java (should be built from scratch)

--- app/src/main/java/com/valvesoftware/android/steam/community/Manifest.java
+++
@@ -1,8 +0,0 @@
-package com.valvesoftware.android.steam.community;
-
-public final class Manifest {
-
-    public static final class permission {
-        public static final String C2D_MESSAGE = "com.valvesoftware.android.steam.community.permission.C2D_MESSAGE";
-    }
-}
